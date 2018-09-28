dcfa2620f9dafa12c88970c6985175c30148fb83
Cheng Min Chi <idv2005@gmail.com>
2018-09-28 12:53:31 +0800

    lb 854

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 import settings
-from loader import get_test_loader
+from loader import get_test_loader, add_depth_channel
 from unet_models import UNetResNet
 from unet_new import UNetResNetV4
 from postprocessing import crop_image, binarize, crop_image_softmax
@@ -30,6 +30,7 @@ def do_tta_predict(model, tta_num=4):
         outputs = None
         with torch.no_grad():
             for i, img in enumerate(test_loader):
+                add_depth_channel(img)
                 img = img.cuda()
                 output, _ = model(img)
                 output = torch.sigmoid(output)
@@ -78,13 +79,13 @@ def ensemble(checkpoints):
         model = model.cuda()
         print('predicting...', checkpoint)
 
-        pred, meta = do_tta_predict(model, tta_num=4)
+        pred, meta = do_tta_predict(model, tta_num=2)
         preds.append(pred)
 
     y_pred_test = generate_preds_softmax(np.mean(preds, 0), (settings.ORIG_H, settings.ORIG_W))
 
     submission = create_submission(meta, y_pred_test)
-    submission_filepath = 'ensemble_res34_0123_tta4.csv'
+    submission_filepath = 'ensemble_depths_res34_12_tta2_1.csv'
     submission.to_csv(submission_filepath, index=None, encoding='utf-8')
 
 def generate_preds(outputs, target_size):
@@ -112,9 +113,15 @@ if __name__ == '__main__':
     #    r'G:\salt\models\152_new\best_0.pth', r'G:\salt\models\152_new\best_1.pth',
     #    r'G:\salt\models\152_new\best_2.pth'
     #]
+    #checkpoints = [ LB841
+    #    r'D:\data\salt\models\UNetResNetV4_34\best_0.pth', r'D:\data\salt\models\UNetResNetV4_34\best_1.pth',
+    #    r'D:\data\salt\models\UNetResNetV4_34\best_2.pth'#, r'D:\data\salt\models\UNetResNetV4_34\best_3.pth'
+    #]
     checkpoints = [
-        r'D:\data\salt\models\UNetResNetV4_34\best_0.pth', r'D:\data\salt\models\UNetResNetV4_34\best_1.pth',
-        r'D:\data\salt\models\UNetResNetV4_34\best_2.pth', r'D:\data\salt\models\UNetResNetV4_34\best_3.pth'
+        #r'D:\data\salt\models\depths\UNetResNetV4_34\best_0.pth', 
+        r'D:\data\salt\models\depths\UNetResNetV4_34\best_1.pth',
+        r'D:\data\salt\models\depths\UNetResNetV4_34\best_2.pth'
+        #, r'D:\data\salt\models\depths\UNetResNetV4_34\best_3.pth'
     ]
     #predict()
     ensemble(checkpoints)
